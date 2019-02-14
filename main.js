7a1633639f2bda59359750b386ffe6e949848689
sdomalik <szymon.domalik@gmail.com>
2019-02-14 14:50:52 +0100

    initialize map in current location with marker

--- main.js
+++ main.js
@@ -1,31 +1,43 @@
-let geo = navigator.geolocation
-let lat
-let lng 
-if(geo){
-    geo.getCurrentPosition(function(location){
-        lat = location.coords.latitude
-        lng = location.coords.longitude
-        console.log('szerokosc ' + Lat)
-        console.log('dlugosc ' + Lng)
 
-        let mapOptions = {
-            center: new google.maps.LatLng(lat,lng)
-        }
-    })   
-}
-else{
+//getts current geolocalization and writes to variables
+function getLocation(onlocationready) {
+  let geo = navigator.geolocation;
+  if (geo) {
+    geo.getCurrentPosition(function (location) {
+      let lat = location.coords.latitude
+      let lng = location.coords.longitude
+
+      onlocationready({
+        lat: lat,
+        lng: lng
+      })
+    })
+  }
+  else {
     console.log('niedostepny')
+  }
 }
 
-function myMap() 
-{
-let mapProp= {
-    center: new google.maps.LatLng(),
-    zoom:5,
+//function display map in current position
+function initMap() {
+  getLocation(function (locationData) {
+    let map = new google.maps.Map(
+      document.getElementById('map'), {
+        zoom: 13,
+        center: locationData,
+        gestureHandling: 'none',
+        zoomControl: false,
+        streetViewControl: false,
+        keyboardShortcuts: false
+      });
+
+      //add marker in the center of map
+    let marker = new google.maps.Marker({ position: locationData, map: map });    
+  })
+
 }
 
-let map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
+
 
 
 
